b8f129e9b0f04be42fa1289184a097779fd8cef4
Murat Eken <mueken@microsoft.com>
2017-03-07 20:34:51 +0000

    first working version...

--- Forward/run.py
+++ Forward/run.py
@@ -1,8 +1,37 @@
-import os
+import os,sys
 import json
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", "env/Lib/site-packages")))
 
-inputMessage = open(os.environ['inputMessage']).read()
+import pydocumentdb.documents as documents
+import pydocumentdb.document_client as document_client
+import requests
+
+
+inputMessage = open(os.environ["inputMessage"]).read()
 event = json.loads(inputMessage)
 
-print(json.dumps(event))
+docDBHost = os.environ["docDBHost"]
+docDBKey  = os.environ["docDBKey"]
+
+client = document_client.DocumentClient(
+    docDBHost, {"masterKey": docDBKey})
+
+docs = list(client.QueryDocuments("dbs/devices/colls/deviceInfo", 
+    "SELECT c.papayaId, c.papayaToken FROM c WHERE c.deviceId = '" + event["deviceid"] + "'"))
+
+if len(docs) > 0:
+    for doc in docs:
+        h = {
+            "Authorization": doc["papayaToken"], 
+            "Content-Type": "application/json",
+            "Cache-Control": "no-cache"
+        }
+        p = {
+            "Urgent": False,
+            "Unit": "washstop",
+            "Amount": "%.1f" % event["totalpoweroutput"]
+        }
+        r = requests.post("https://www.papayagogo.com/%s/event" % doc["papayaId"], 
+            headers=h, data=json.dumps(p))
+        print r.text
