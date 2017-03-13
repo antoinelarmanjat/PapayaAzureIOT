dcb34e1b2e419f57984bb0d966f0eb7af3dd90ac
Murat Eken <eken.murat@outlook.com>
2017-03-13 13:12:07 +0100

    handling the papaya response v1...

--- Lib/papaya.py
+++ Lib/papaya.py
@@ -11,8 +11,10 @@ class Papaya:
     def register(self, deviceId, devicePublicKey, payerId, payeeId, payeeToken):
         headers = { "Authorization": payeeToken, "Content-Type": "application/json", "Cache-Control": "no-cache" }
         payload = { "ExternalId": deviceId, "PublicKey": devicePublicKey, "DeviceType": "WindTurbine"}
-        return requests.post("https://www.papayagogo.com/%s/%s/devices" % (payeeId, payerId),
+        r = requests.post("https://www.papayagogo.com/%s/%s/devices" % (payeeId, payerId),
             headers=headers, data=json.dumps(payload))
+        print r.text
+        return r
 
     def update(self, id, token, payload):
         headers = { "Authorization": token, "Content-Type": "application/json", "Cache-Control": "no-cache" }
