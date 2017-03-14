935660defdfc128db9459824ef8369f63648e7ec
Murat Eken <eken.murat@outlook.com>
2017-03-14 11:22:35 +0100

    getting the list of devices from iot hub...

--- Simulate/run.py
+++ Simulate/run.py
@@ -7,13 +7,12 @@ import sys
 # adding the shared libraries path, must be done before the libraries are imported
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", "Lib")))
 # shared libraries
-from repository import repository
 from iothub import iothub
 
 AVG_WINDSPEED = 10 # m/s
 AVG_POWEROUTPUT = 800 #kW
 
-devices = repository.list_devices()
+devices = iothub.list_devices()
 for id, pk in devices:
     currWindSpeed = AVG_WINDSPEED + random.random() * 4 - 2
     currPowerOutput = AVG_POWEROUTPUT + random.random() * 200 - 100
